d6e92e57b20b48db057632207d68f36e49bc22c1
Olya Dovzhenko <kalita.sun1982@gmail.com>
2024-10-29 21:33:49 +0200

    Lesson-24

--- Lesson-22/tasks/scripts/task19.js
+++ Lesson-22/tasks/scripts/task19.js
@@ -14,3 +14,16 @@ function filtrString() {
 window.onload = () => {
   document.querySelector('.myInput').onchange = filtrString
 }
+
+// const availableDays = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"]
+
+// const day = prompt("Day:")
+
+// try {
+//   if (!availableDays.includes(day) && !/^[0-6]$/.test(day))
+//     throw new Error("Error! Day is not correct")
+//   if (/^([06]|sat|sun)$/.test(day)) console.log("This is weekend")
+//   else console.log("This is not weekend")
+// } catch (error) {
+//   alert(error.message)
+// }
---
+++ Lesson-24/scripts/task.js
@@ -0,0 +1,57 @@
+class ImageRequest {
+  constructor(apiUrl) {
+    this.apiUrl = apiUrl
+  }
+
+  async getImageUrl() {
+    return fetch(this.apiUrl)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        return data.url
+      })
+  }
+}
+
+class ImageGallery {
+  constructor(imagesList) {
+    this.imagesList = imagesList
+  }
+  render() {
+    const imageGallery = document.createElement('ul')
+    imageGallery.className = 'imageGallery'
+    for (let i = 0; i < this.imagesList.length; i++) {
+      const imageItem = document.createElement('li')
+      imageItem.className = 'imageItem'
+      const randomImage = document.createElement('img')
+      randomImage.setAttribute('src', this.imagesList[i])
+      randomImage.classList = 'randomImage'
+      imageItem.append(randomImage)
+      console.log(imageItem)
+      imageGallery.append(imageItem)
+    }
+    document.querySelector('.resultContainer').append(imageGallery)
+  }
+}
+
+async function getArray(imageNumber, imageRequest) {
+  const arr = new Array(imageNumber)
+
+  for (let i = 0; i < arr.length; i++) {
+    const randomUrl = await imageRequest.getImageUrl()
+    if (!/(\.jpg|\.jpeg|\.gif|\.png)/.test(randomUrl.toLowerCase())) {
+      i--
+      continue
+    }
+    arr[i] = randomUrl
+  }
+  return arr
+}
+
+window.onload = async () => {
+  const imageRequest = new ImageRequest('https://random.dog/woof.json')
+  const imagesList = await getArray(9, imageRequest)
+  const imageGallery = new ImageGallery(imagesList)
+  imageGallery.render()
+}
